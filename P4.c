673d6a06506090c19981c60e5842de8a38447da5
Matheus Pedrosa <37350597+MP3drosa@users.noreply.github.com>
2018-05-09 11:17:28 -0300

    Add files via upload

---
+++ P4.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main()
+{
+	char s[256];
+	int i, cum = 0;
+	scanf("%s", s);
+	for(i = 0; s[i] != '\0'; i++)
+	{
+	  if( s[i] >=48 && s[i] <= 57)
+	  {
+         s[i] = s[i] - '0';
+         cum = cum*10 + s[i];
+      }
+    }
+    printf("O numero digitado eh : %i", cum);
+    
+    return 0;
+}
