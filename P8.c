a84cfc27f524e2354f9b7be4e5771c6c59656592
Matheus Pedrosa <37350597+MP3drosa@users.noreply.github.com>
2018-06-28 01:48:29 -0300

    Update P8.c

--- P8.c
+++ P8.c
@@ -6,44 +6,41 @@
 
 void gera_numeros(float * vetor, int tam) 
 {
-    int i;
-    float num;
-    for(i = 0; i < tam; i++)
-        vetor[i] =  (float) rand()/(float) RAND_MAX + 0.5f;
-}
- 
-
-float somatorio( float vetor[], int i )
-{ 
-    float soma = 0;
-    if (i < 0) 
-        return soma;
-    return vetor[i] + somatorio(vetor, i - 1);
+	int i;
+	float num;
+	for(i = 0; i < tam; i++)
+		vetor[i] = (float) rand()/(float) RAND_MAX + 0.5f;
 }
 
-float produtorio( float vetor[], int  i)
+float reduce(float * pini, float * pfim,
+		float (*funcao)(float, float)) 
 {
-	float prod = 1;
-	if(i < 0)
-	    return prod;
-    return vetor[i] * produtorio(vetor, i - 1);	    
+	return (pini == pfim)? 0 :
+		(*funcao)(*pini, reduce(pini+1, pfim, funcao));
+}
+float soma(float a, float b) 
+{
+	return a+b;
+}
+float produto(float a, float b) 
+{
+	return a*b;
 }
 
-int main()
+int main ()
 {
-	int aux;
-	float vetor[99], r;
 	srand(123456);
-	gera_numeros(vetor,99);
-	printf("1-SOMATORIO\n\n2-PRODUTORIO\n\n");
-	scanf("%d", &aux);
+	float vetor[100];
+	int i;
+	gera_numeros(vetor, 100);
+	printf("Digite 1 para calcular o Somatorio e 2 para calcular o Produtorio: ");
+	scanf("%d", &i);
 	getchar();
-	if(aux == 1)
-	    r = somatorio(vetor,99);
-	else if( aux == 2)
-	    r = produtorio(vetor,99);
-	printf("\n\n%.2f", r);
+	if(i==1)
+		printf("\nSomatorio = %f", reduce(vetor, vetor+100, soma));
+	else if(i==2)
+		printf("\nProdutorio = %f", reduce(vetor, vetor+100, produto));
+	else
+		printf("\nOpcao invalida!");
 	return 0;
-
-	
 }
